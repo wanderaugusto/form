bdec6b144ffd5b68251a7140a79c10f21c2393e5
wanderaugusto <95436393+wanderaugusto@users.noreply.github.com>
2023-10-09 18:54:58 -0300

    upt2

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,8 +3,8 @@ import './home.css'
 function Home() {
   return (
    <div className='cont1'>
-    <a href="/acard"><input type="button" value="Pagina do Card" /></a>
-    <a href="/aform"><input type="button" value="Pagina do Formulário" /></a>
+    <a href="https://desafio-react-henna.vercel.app/acard"><input type="button" value="Pagina do Card" /></a>
+    <a href="https://desafio-react-henna.vercel.app/aform"><input type="button" value="Pagina do Formulário" /></a>
    </div>
   )
 }
